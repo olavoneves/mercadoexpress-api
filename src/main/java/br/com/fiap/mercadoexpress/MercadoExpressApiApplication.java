32f0187107075b2513487758e4214b0326817fab
Olavo Neves <olavo9neves@gmail.com>
2026-08-11 20:08:03 -0300

    feat: add api base

---
+++ src/main/java/br/com/fiap/mercadoexpress/MercadoExpressApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mercadoexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoExpressApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoExpressApiApplication.class, args);
+	}
+
+}
